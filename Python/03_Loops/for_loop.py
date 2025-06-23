6e24dad48b4cc63d74e39e25236b21871a6e792b
Akshit203 <akshitgupta2003.ak@gmail.com>
2025-06-23 22:23:56 +0530

    .

---
+++ Python/03_Loops/for_loop.py
@@ -0,0 +1,22 @@
+# for loop 
+
+# Example 1 : using range
+x = 5
+
+for i in range(0, x):
+    print(i)
+
+for i in range(5): # 0 to 4
+    print(i)
+
+# Example 2 : Loop through a string
+
+for i in "Chandigarh":
+    print(i)
+
+# Example 3 :
+
+list = ["A", "B", "C", "D"]
+
+for i in list:
+    print(i)
---
+++ Python/03_Loops/while_loop.py
@@ -0,0 +1,37 @@
+# while loop 
+
+num = 0
+
+while (num < 3):
+    num += 1
+    print("Number : ", num)
+
+
+# while loop with break and continue :
+
+count = 0
+
+while (count < 5):
+    count += 1
+    if (count == 3):
+        continue
+    print(count)
+
+
+count2 = 0
+
+while (count2 < 5):
+
+    if (count == 3):
+        break
+    print(count2)
+    count2 += 1
+
+    
+# Infinite while loop 
+
+while True :
+    print("xyz")
+
+
+
